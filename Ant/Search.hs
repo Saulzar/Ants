5efc5d80d4aaf1cb3be6fb955ba2992f15f9759c
Oliver Batchelor <saulzar@gmail.com>
2011-12-05 22:59:36 +0800

    Add search source

---
+++ Ant/Search.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE PatternGuards #-}
+
+module Ant.Search
+    ( SearchNode (..)
+    , search
+    
+    )
+    
+    where
+
+
+import qualified Data.IntSet as S
+import Data.Maybe
+
+import Data.PSQueue ( Binding(..) )
+import qualified Data.PSQueue as Q    
+    
+
+type Queue m = Q.PSQ SearchNode m
+
+data SearchNode = SearchNode 
+    { snKey      :: Int
+    , snDistance :: Int
+    , snPred     :: Maybe (SearchNode)
+    }
+    
+instance Show (SearchNode) where
+    show (SearchNode r d _) = show r ++ " dist: " ++ show d
+
+    
+instance Eq (SearchNode) where
+    (==) sn sn' = snKey sn == snKey sn'
+    
+instance Ord (SearchNode) where
+    compare sn sn' = snKey sn `compare` snKey sn'
+    
+
+
+insertMin :: (Ord m) => SearchNode -> m -> Queue m -> Queue m
+insertMin node p = Q.alter alter node where
+    alter (Just p') = Just (min p p')
+    alter Nothing    = Just p
+{-# INLINE insertMin #-}    
+    
+    
+    
+search :: (Ord m) => (SearchNode -> [SearchNode]) -> (SearchNode -> m) -> Int -> [SearchNode]
+search succ metric r = search (S.singleton r) (Q.singleton node0 (metric node0)) where
+    node0 = SearchNode r 0 Nothing
+          
+    search seen queue  | Nothing                      <- view = []
+                       | Just ((node :-> _), queue')  <- view = next node seen queue'                    
+        where view = Q.minView queue  
+        
+    next node seen queue = node : (search seen' queue')
+        where
+            queue'  = foldr insert queue successors
+            seen'   = S.insert (snKey node) seen
+            
+            successors = filter (flip S.notMember seen . snKey) (succ node)
+            insert node = insertMin node (metric node)
